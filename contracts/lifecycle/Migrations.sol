caa834b5adf518ce29fb53602326907d6881392a
OmarMWarraich <earthling986@gmail.com>
2023-01-05 01:45:50 +0500

    chore: Setup IDE for Truffle Development

---
+++ contracts/lifecycle/Migrations.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title Migrations
+ * @dev This contract keeps track of the migrations that have been run on the network.
+ */
+
+contract Migrations is Ownable {
+  uint256 public lastCompletedMigration;
+
+  function setCompleted(uint256 completed) public onlyOwner {
+    lastCompletedMigration = completed;
+  }
+
+  function upgrade(address newAddress) public onlyOwner {
+    Migrations upgraded = Migrations(newAddress);
+    upgraded.setCompleted(lastCompletedMigration);
+  }
+}

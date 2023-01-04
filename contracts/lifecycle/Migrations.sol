99043fd221bd661b7ca5d56a50c3a3963c2bb0cd
OmarMWarraich <earthling986@gmail.com>
2023-01-05 03:19:07 +0500

    chore: Add Dependencies

--- contracts/lifecycle/Migrations.sol
+++ contracts/lifecycle/Migrations.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title Migrations
